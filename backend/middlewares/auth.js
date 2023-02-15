ea3ce1a5c40e15404b9b3d859b17600642839a64
Mark Politiko <politikomark@yandex.ru>
2023-02-15 23:53:48 +0300

    First commit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -18,10 +18,10 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
 
-  next(); // пропускаем запрос дальше
+  return next(); // пропускаем запрос дальше
 };
