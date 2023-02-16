9388e444ae8cfd7fa32dca9717420a08b4f5d9aa
Mark Politiko <politikomark@yandex.ru>
2023-02-16 18:25:32 +0300

    First commit

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -49,15 +49,17 @@ function Main({
         </section>
 
         <section className="elements">
-          {cards.map((card) => (
-            <Card
-              key={card._id}
-              card={card}
-              onCardClick={onCardClick}
-              onCardLike={onCardLike}
-              onCardDelete={onCardDelete}
-            />
-          ))}
+          {cards.map((card) => {  // ({ _id, ...card })
+            return (
+              <Card
+                key={card._id}
+                card={card}
+                onCardClick={onCardClick}
+                onCardLike={onCardLike}
+                onCardDelete={onCardDelete}
+              />
+            );
+          })}
         </section>
       </div>
     </>
